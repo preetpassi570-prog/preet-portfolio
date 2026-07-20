bdc00820cff050964e3e9888af7ae3a70afd6f90
Preet Passi <preetpassi570@gmail.com>
2026-07-20 02:43:37 -0700

    Fixed responsive design

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   server: {
@@ -7,6 +8,14 @@ export default defineConfig({
     watch: {
       usePolling: true,
       ignored: ['**/images/**']
+    }
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        404: resolve(__dirname, '404.html')
+      }
     }
   }
 });
